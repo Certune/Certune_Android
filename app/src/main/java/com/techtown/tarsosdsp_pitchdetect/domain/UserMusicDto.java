d9b183637157a5c8077f00156b31c92ef5594e40
ceppetto <pcy07111@gmail.com>
2022-01-27 15:30:46 +0900

    feat(MainActivity):voice to DB by line

--- app/src/main/java/com/techtown/tarsosdsp_pitchdetect/domain/UserMusicDto.java
+++ app/src/main/java/com/techtown/tarsosdsp_pitchdetect/domain/UserMusicDto.java
@@ -1,20 +1,20 @@
 package com.techtown.tarsosdsp_pitchdetect.domain;
 
+import java.util.ArrayList;
+
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 public class UserMusicDto {
     public String start_time;
-    public String end_time;
-    public String note;
-    public String grade;
+    public ArrayList<String> note;
+    public String score;
 
     @Builder
-    public UserMusicDto(String cumul_time, String note){
+    public UserMusicDto(String start_time, ArrayList<String> note, String score){
         this.start_time = start_time;
-        this.end_time = end_time;
         this.note = note;
-        this.grade = grade;
+        this.score = score;
     }
 }

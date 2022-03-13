a2f423820a7b32d8af5fcc933181339331686f5f
ceppetto <pcy07111@gmail.com>
2022-03-13 16:07:08 +0900

    feat(RegisterActivity): add user DB

--- app/src/main/java/com/techtown/tarsosdsp_pitchdetect/RegisterActivity.java
+++ app/src/main/java/com/techtown/tarsosdsp_pitchdetect/RegisterActivity.java
@@ -1,11 +1,14 @@
 package com.techtown.tarsosdsp_pitchdetect;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,9 +17,15 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
@@ -81,8 +90,29 @@ public class RegisterActivity extends AppCompatActivity {
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                        Map<String, Object> user = new HashMap<>();
+                        user.put("name", name);
+                        user.put("sex", sex);
+
                         if (task.isSuccessful()) {
                             // 회원가입 성공시
+                            db.collection("User").document(email)
+                                    .set(user)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w(TAG, "Error writing document", e);
+                                        }
+                                    });
+
                             Intent intent = new Intent(RegisterActivity.this, LogInActivity.class);
                             startActivity(intent);
                             finish();

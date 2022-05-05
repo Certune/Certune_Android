fa436caa084fa8e7b0d1f26f03417fde6682de99
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-05-05 21:35:50 +0900

    move(CustomSongList): add lombok annotations and move to dto folder

---
+++ app/src/main/java/com/techtown/tarsosdsp_pitchdetect/domain/CustomSongListDto.java
@@ -0,0 +1,12 @@
+package com.techtown.tarsosdsp_pitchdetect.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomSongListDto {
+    private String indexText;
+    private String songText;
+    private String singerText;
+}

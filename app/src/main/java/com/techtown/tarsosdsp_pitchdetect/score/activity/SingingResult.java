7dc87704b3be6436e996e0fb0e0c381537fb0fbe
ceppetto <pcy07111@gmail.com>
2022-05-15 15:48:18 +0900

    add new folder for result view

---
+++ app/src/main/java/com/techtown/tarsosdsp_pitchdetect/score/activity/SingingResult.java
@@ -0,0 +1,14 @@
+package com.techtown.tarsosdsp_pitchdetect.score.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingingResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singing_result);
+    }
+}

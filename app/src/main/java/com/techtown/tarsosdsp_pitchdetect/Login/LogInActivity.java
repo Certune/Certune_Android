9a90d6a9f218b09581097e336cf1bd011ec1ca32
ceppetto <pcy07111@gmail.com>
2022-05-15 18:52:15 +0900

    feat(LoginActivity): fix button double click problem

--- app/src/main/java/com/techtown/tarsosdsp_pitchdetect/Login/LogInActivity.java
+++ app/src/main/java/com/techtown/tarsosdsp_pitchdetect/Login/LogInActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;

e82bb81eaccf17c0ebb81bd6b2224517c642f27f
emili5 <anjeongmin05@ewhain.net>
2022-05-16 20:59:03 +0900

    feat(CustomWeakSentenceListDto)

---
+++ app/src/main/java/com/techtown/tarsosdsp_pitchdetect/global/CustomWeakSentenceListDto.java
@@ -0,0 +1,14 @@
+package com.techtown.tarsosdsp_pitchdetect.global;
+
+import android.graphics.drawable.Drawable;
+
+import lombok.Builder;
+import lombok.Getter;
+
+
+
+@Getter
+@Builder
+public class CustomWeakSentenceListDto {
+    private String sentenceText;
+}

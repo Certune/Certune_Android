527a7830130bcc846dc7f53a2052688d03730398
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-05-18 19:26:35 +0900

    feat: modify data loading logic

--- app/src/main/java/com/techtown/tarsosdsp_pitchdetect/Singing/activity/LiveSingingActivity.java
+++ app/src/main/java/com/techtown/tarsosdsp_pitchdetect/Singing/activity/LiveSingingActivity.java
@@ -89,74 +89,6 @@ public class LiveSingingActivity extends AppCompatActivity {
         gridLayout.addView(cell, param);
     }
 
-    public void setThis() {
-        // https://stackoverflow.com/questions/35924875/how-to-use-gridlayout-spec-for-a-column-extending-multiple-rows
-        // face view takes 2 rows, 1 column -- zero index based
-        GridLayout.Spec faceRow = GridLayout.spec(0, 2); // starts row 0, takes 2 rows
-        GridLayout.Spec faceCol = GridLayout.spec(0); // starts col 0, takes 1 col
-
-        GridLayout.Spec titleRow = GridLayout.spec(0); // starts row 0, takes 1 row
-        GridLayout.Spec titleCol = GridLayout.spec(1, 3); // starts col 1, takes 3 cols
-
-        GridLayout.Spec plusRow = GridLayout.spec(1); // starts row 1, takes 1 row
-        GridLayout.Spec plusCol = GridLayout.spec(1); // starts col 1, takes 1 col
-
-        GridLayout.Spec minusRow = GridLayout.spec(1); // starts row 1, takes 1 row
-        GridLayout.Spec minusCol = GridLayout.spec(2); // starts col 1, takes 1 col
-
-        GridLayout.Spec checkRow = GridLayout.spec(1); // starts row 1, takes 1 row
-        GridLayout.Spec checkCol = GridLayout.spec(3); // starts col 1, takes 1 col
-
-// create the LayoutParams using our row/col for each view
-        GridLayout.LayoutParams faceParams = new GridLayout.LayoutParams(faceRow, faceCol);
-        faceParams.setGravity(Gravity.FILL_VERTICAL); // fill vertical so we take up the full 2 rows
-// dummy text views to fill some space
-        TextView faceText = new TextView(this);
-        faceText.setPadding(32, 32, 32, 32); // add some random padding to make the views bigger
-        faceText.setLayoutParams(faceParams);
-        faceText.setText("FACE");
-        faceText.setGravity(Gravity.CENTER);
-        faceText.setBackgroundColor(Color.RED);
-        gridLayout.addView(faceText, faceParams);
-
-        GridLayout.LayoutParams titleParams = new GridLayout.LayoutParams(titleRow, titleCol);
-        titleParams.setGravity(Gravity.FILL_HORIZONTAL); // fill horizontal so we take up the full 3 columns
-        TextView titleText = new TextView(this);
-        titleText.setPadding(32, 32, 32, 32);
-        titleText.setLayoutParams(titleParams);
-        titleText.setText("TITLE");
-        titleText.setGravity(Gravity.CENTER);
-        titleText.setBackgroundColor(Color.BLUE);
-        gridLayout.addView(titleText, titleParams);
-
-        GridLayout.LayoutParams minusParams = new GridLayout.LayoutParams(minusRow, minusCol);
-        TextView minusText = new TextView(this);
-        minusText.setPadding(32, 32, 32, 32);
-        minusText.setLayoutParams(minusParams);
-        minusText.setText("MIN");
-        minusText.setGravity(Gravity.CENTER);
-        minusText.setBackgroundColor(Color.YELLOW);
-        gridLayout.addView(minusText, minusParams);
-
-        GridLayout.LayoutParams plusParams = new GridLayout.LayoutParams(plusRow, plusCol);
-        TextView plusText = new TextView(this);
-        plusText.setPadding(32, 32, 32, 32);
-        plusText.setLayoutParams(plusParams);
-        plusText.setText("PLS");
-        plusText.setGravity(Gravity.CENTER);
-        plusText.setBackgroundColor(Color.GREEN);
-        gridLayout.addView(plusText, plusParams);
-
-        GridLayout.LayoutParams checkParams = new GridLayout.LayoutParams(checkRow, checkCol);
-        TextView checkText = new TextView(this);
-        checkText.setPadding(32, 32, 32, 32);
-        checkText.setLayoutParams(faceParams);
-        checkText.setText("CHK");
-        checkText.setGravity(Gravity.CENTER);
-        checkText.setBackgroundColor(Color.MAGENTA);
-        gridLayout.addView(checkText, checkParams);
-    }
-
     public void settingView() {
         int songLowKeyIdx = noteToIdx(songLowKey);
         int songHighKeyIdx = noteToIdx(songHighKey);
@@ -198,8 +130,7 @@ public class LiveSingingActivity extends AppCompatActivity {
                         setScrollSettings();
                         List list = (List) Objects.requireNonNull(document.getData()).get("sentence");
 
-                        // TODO : i 최종 값 수정해야 함(현재는 든 게 없어서 empty String 에러
-                        for (int i = 0; i < 1; i++) {
+                        for (int i = 0; i < list.size(); i++) {
                             HashMap<String, ArrayList<HashMap<String, Object>>> map = (HashMap) list.get(i);
                             ArrayList<HashMap<String, Object>> arrayMap = (ArrayList<HashMap<String, Object>>) map.get("notes");
 

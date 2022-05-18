0dd4cb3babe8e15cd66c17b960282630e4f579a3
Han Park <han@deeplyinc.com>
2022-05-18 19:45:32 +0900

    feat: improve data fetching and listing by changing the way adapters are used

--- app/src/main/java/com/techtown/tarsosdsp_pitchdetect/SongListActivity.java
+++ app/src/main/java/com/techtown/tarsosdsp_pitchdetect/SongListActivity.java
@@ -1,17 +1,22 @@
 package com.techtown.tarsosdsp_pitchdetect;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.techtown.tarsosdsp_pitchdetect.global.CustomSongListDto;
+
 public class SongListActivity extends AppCompatActivity {
+    private static final String TAG = "SongListActivity";
+
     private ListView list;
     private SongListViewAdapter adapter;
 
@@ -20,16 +25,34 @@ public class SongListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_list);
 
-        adapter = new SongListViewAdapter();
-        adapter.getData();
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                adapter.addItem();
+        list = (ListView) findViewById(R.id.songListArea); // TODO: Adapter 설정을 해주어야 하니 먼저 이렇게 view 를 얻고...
 
-                list = (ListView) findViewById(R.id.songListArea);
-                list.setAdapter(adapter);
-            }
-        }, 2500);
+        adapter = new SongListViewAdapter(); // TODO: Adapter를 만들어서
+        list.setAdapter(adapter); // TODO: 바로 먼저 적용해버리면 돼요.
+
+        getData(); // TODO: 그리고 데이터를 가져오는 함수를 실행.
+    }
+
+    // TODO: 이 함수는 나중에 MVVM을 적용하시게 되면 repository (data source) 등으로 빼는게 좋아요. Activity 에서 할 작업은 아니라서..
+    private void getData() {
+        // Song 컬렉션 내에 위치한 모든 곡 이름 가져오기
+        FirebaseFirestore.getInstance().collection("Song")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "onComplete()");
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                CustomSongListDto dto = new CustomSongListDto();
+                                dto.setSongText(document.getId());
+                                dto.setSingerText(document.getData().get("singer").toString());
+                                adapter.addItem(dto); // TODO: 이렇게 adapter 에 추가하면 돼요.
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/techtown/tarsosdsp_pitchdetect/global/CustomSongListDto.java
+++ app/src/main/java/com/techtown/tarsosdsp_pitchdetect/global/CustomSongListDto.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CustomSongListDto {
-    private String indexText;
+    private String indexText; // TODO: index 가 Song 이라는 객체(class)의 고유한 속성(property, member variable)이라고 볼 수 없을 것 같아서, 그러면 여기서는 빠지는게 맞는 것 같아요.
     private String songText;
     private String singerText;
 }

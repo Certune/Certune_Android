bfddac366488da1dfe35a81114c4fa57c923ade2
ceppetto <pcy07111@gmail.com>
2022-05-20 09:25:32 +0900

    feat(SingingResult): fix progress bar design

--- app/src/main/java/com/techtown/tarsosdsp_pitchdetect/score/activity/SingingResult.java
+++ app/src/main/java/com/techtown/tarsosdsp_pitchdetect/score/activity/SingingResult.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.techtown.tarsosdsp_pitchdetect.R;
 
 public class SingingResult extends AppCompatActivity {
+
+    private static FirebaseFirestore database = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

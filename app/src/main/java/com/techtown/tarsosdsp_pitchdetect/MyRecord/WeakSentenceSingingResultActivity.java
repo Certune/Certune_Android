b3caa9728af3e6c5aa19ea7eeec098651fed2398
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-06-05 01:42:43 +0900

    feat: add note, rhythm score to weakSentenceSingingResult

--- app/src/main/java/com/techtown/tarsosdsp_pitchdetect/MyRecord/WeakSentenceSingingResultActivity.java
+++ app/src/main/java/com/techtown/tarsosdsp_pitchdetect/MyRecord/WeakSentenceSingingResultActivity.java
@@ -6,16 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.techtown.tarsosdsp_pitchdetect.R;
 
 public class WeakSentenceSingingResultActivity extends AppCompatActivity {
 
-    Button scoreTextButton;
+    Button totalscoreButton;
     Button endButton;
+    TextView noteScoreTextView;
+    TextView rhythmScoreTextView;
+    ProgressBar noteScoreProgressbar;
+    ProgressBar rhythmScoreProgressbar;
 
-    String songName;
-    String sentenceIdx;
     String noteScore;
     String rhythmScore;
     String totalScore;
@@ -30,11 +34,23 @@ public class WeakSentenceSingingResultActivity extends AppCompatActivity {
         rhythmScore = subIntent.getStringExtra("rhythmScore");
         totalScore = subIntent.getStringExtra("totalScore");
 
-        scoreTextButton = findViewById(R.id.scoreTextButton);
+        totalscoreButton = findViewById(R.id.scoreTextButton);
+        noteScoreTextView = findViewById(R.id.noteScoreTextView);
+        rhythmScoreTextView = findViewById(R.id.rhythmScoreTextView);
+        noteScoreProgressbar = findViewById(R.id.note_progressbar);
+        rhythmScoreProgressbar = findViewById(R.id.rhythm_progressbar);
         endButton = findViewById(R.id.endButton);
 
-        scoreTextButton.setText(totalScore);
-        
+        String totalScoreText = Math.round(Double.parseDouble(totalScore)) +"점";
+        String noteScoreText = Math.round(Double.parseDouble(noteScore)) +"점";
+        String rhythmScoreText = Math.round(Double.parseDouble(rhythmScore)) +"점";
+
+        totalscoreButton.setText(totalScoreText);
+        noteScoreTextView.setText(noteScoreText);
+        rhythmScoreTextView.setText(rhythmScoreText);
+        noteScoreProgressbar.setProgress((int) Math.round(Double.parseDouble(noteScore)));
+        rhythmScoreProgressbar.setProgress((int) Math.round(Double.parseDouble(rhythmScore)));
+
         endButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
